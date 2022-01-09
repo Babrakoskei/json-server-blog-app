15dca9ce28ce4336781ee68e6d52b2b19864c3d7
Babra Koskei <babra.koskei@churpy.co>
2022-01-10 00:38:11 +0300

    k

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-// const port = process.env.PORT || 3018;
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+const port = process.env.PORT || 3000;
 
 server.use(middlewares);
 server.use(router);
