0e4880c3ab2a81f1825463672b452290956eebe3
Babra Koskei <babra.koskei@churpy.co>
2022-01-10 00:53:19 +0300

    k

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 
+const app = jsonServer();
+app.listen(3000,()=>
+console.log('starting server: http://localhost:3000')
+);
 server.use(middlewares);
 server.use(router);
 
